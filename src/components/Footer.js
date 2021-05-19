12deee432d9394246954671f16a9399c725ec58d
Hossam Marey <hossam.marey@rytalo.com>
2021-05-20 01:52:37 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,29 @@
 import React from "react";
+import gitImg from "../assets/github.png";
+import linkedinImg from "../assets/linkedin.png";
 
 const Footer = () => {
-  return <footer>hossam marey</footer>;
+  return (
+    <footer>
+      <div className="content">
+        <span> © Hossam Marey</span>
+        <a
+          href="https://www.linkedin.com/in/hossam-marey/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedinImg} alt="LinkedIN" />
+        </a>
+        <a
+          href="https://github.com/HossamMarey/dotsless"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={gitImg} alt="Github" />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
